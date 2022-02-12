03829da1e1f45ccbaaa9ebb960ab850b46521a0f
Joshua Vasquez <seeifyoucanmeetme@gmail.com>
2022-02-11 16:46:41 -0800

    adding wrapper for class

---
+++ software/python_control_software/pareto_leg/pareto_leg.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""Driver for controlling leg position"""
+
+from pareto_leg.five_bar_kins import FiveBarKinematics2D
+from pareto_leg.odrive_driver import odrive_driver
+
+
+class ParetoLeg(object):
+
+    def __init__(self, odrive, l1_len, l2_len):
+        """constructor. Assumes odrive motors have already been pre-configured."""
+        self.odd = OdriveDriver(odrive)
+        self.kins = FiveBarKinematics2D(l1_len, l2_len)
+
+
+    def set_position(self, x, y):
+        """Set the position of the robot leg in cartesian coordinates."""
+        theta_axis0, theta_axis1 = self.kins.cartesian_to_joint(x, y)
+        self.odd.set_positions(theta_axis0, theta_axis1)
+
+
+    def get_position(self):
+        """Get the position of the robot leg in cartesian coordinates."""
+        theta_axis0, theta_axis1 = self.odd.get_positions()
+        return self.kins.joint_to_cartesian(theta_axis0, theta_axis1)
